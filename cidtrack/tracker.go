99ec614b32029b0d17426195bea2d1b1a2784e59
Tomasz Zdybał <tomek@zdybal.lap.pl>
2020-09-22 01:01:05 +0200

    Initial version of plugin

---
+++ cidtrack/tracker.go
@@ -0,0 +1,40 @@
+package cidtrack
+
+import cid "github.com/ipfs/go-cid"
+
+type tracker struct {
+	recv chan stat
+	sent chan stat
+
+	recvStats map[cid.Cid]uint64
+	sentStats map[cid.Cid]uint64
+}
+
+func newTracker() *tracker {
+	return &tracker{
+		recv:      make(chan stat),
+		sent:      make(chan stat),
+		recvStats: make(map[cid.Cid]uint64),
+		sentStats: make(map[cid.Cid]uint64),
+	}
+}
+
+func (t *tracker) run() {
+	for {
+		// TODO(tzdybal): gently exit
+		select {
+		case s := <-t.recv:
+			t.recvStats[s.cid] += uint64(s.size)
+		case s := <-t.sent:
+			t.sentStats[s.cid] += uint64(s.size)
+		}
+	}
+}
+
+func (t *tracker) recvChan() chan stat {
+	return t.recv
+}
+
+func (t *tracker) sentChan() chan stat {
+	return t.sent
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/tzdybal/cidtrack
+
+go 1.15
+
+require (
+	github.com/ipfs/go-bitswap v0.2.19
+	github.com/ipfs/go-cid v0.0.7
+	github.com/ipfs/go-ipfs v0.6.0
+	github.com/ipfs/go-log v1.0.4
+	github.com/libp2p/go-libp2p-peer v0.2.0
+)
+
+replace github.com/ipfs/go-bitswap => github.com/tzdybal/go-bitswap v0.2.21-0.20200921202138-3f29c3ae78dc
